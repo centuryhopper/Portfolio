b2eec700a7a4a6219e51faf211fda0f759d83ee0
centuryhopper <43508492+centuryhopper@users.noreply.github.com>
2024-06-27 17:09:44 -0400

    users can now send messages to me for reaching out

--- Controllers/ContactsController.cs
+++ Controllers/ContactsController.cs
@@ -1,7 +1,10 @@
 
+using System.Security.Claims;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 using Portfolio.Entities;
 using Portfolio.Repositories;
+using Portfolio.Utilities;
 
 
 namespace Portfolio.Controllers;
@@ -11,28 +14,35 @@ public class ContactsController : Controller
 {
     private readonly IContactsDataRepository<ContactMeModel> contactRepo;
     private readonly ILogger<ContactsController> logger;
+    private readonly IConfiguration configuration;
 
-    public ContactsController(IContactsDataRepository<ContactMeModel> contactRepo, ILogger<ContactsController> Logger)
+    public ContactsController(IContactsDataRepository<ContactMeModel> contactRepo, ILogger<ContactsController> Logger, IConfiguration configuration)
     {
         this.contactRepo = contactRepo;
         logger = Logger;
+        this.configuration = configuration;
     }
 
     public IActionResult Index()
     {
-        return View();
+        var previousInfo = HttpContext.Session.GetString("CONTACT") ?? "";
+
+        ContactMeModel? vm = string.IsNullOrEmpty(previousInfo) ? null : JsonConvert.DeserializeObject<ContactMeModel>(previousInfo);
+
+        HttpContext.Session.Remove("CONTACT");
+        return View(vm);
     }
 
-    [HttpGet]
-    [Route("get-contacts")]
-    public async Task<IActionResult> GetAsync()
-    {
-        var contacts = await contactRepo.GetContactsAsync();
-        return Ok(contacts);
-    }
+    // [HttpGet]
+    // [Route("get-contacts")]
+    // public async Task<IActionResult> GetAsync()
+    // {
+    //     var contacts = await contactRepo.GetContactsAsync();
+    //     return Ok(contacts);
+    // }
 
     [HttpPost]
-    public async Task<IActionResult> Index(ContactMeModel model)
+    public async Task<IActionResult> Index(ContactMeModel vm)
     {
         /*
         {
@@ -42,13 +52,38 @@ public class ContactsController : Controller
         "message": "ree hee hee rooby shnacks!"
         }
         */
-        var data = await contactRepo.PostDataAsync(model);
+        if (!ModelState.IsValid)
+        {
+            var errors = Helpers.GetErrors<ContactsController>(ModelState);
+            TempData[TempDataKeys.ALERT_ERROR] = string.Join("$$$", errors);
+
+            HttpContext.Session.SetString("CONTACT", JsonConvert.SerializeObject(vm, Formatting.Indented));
+            return RedirectToAction(nameof(Index));
+        }
+
+        var data = await contactRepo.PostDataAsync(vm);
 
         if (!data.flag)
         {
-            return BadRequest(data);
+            TempData[TempDataKeys.ALERT_ERROR] = data.message;
+            HttpContext.Session.SetString("CONTACT", JsonConvert.SerializeObject(vm, Formatting.Indented));
+            return RedirectToAction(nameof(Index));
         }
 
-        return Ok(data);
+        var smtpInfo = configuration.GetConnectionString("smtp_client").Split("|");
+
+        var userEmail = User.FindFirstValue(ClaimTypes.Name);
+
+        Helpers.SendEmail(subject: "email from an potential business partner", senderEmail: smtpInfo[0], senderPassword: smtpInfo[1], body: @$"
+        Name: {vm.Name}
+        Email: {vm.Email}
+        Subject: {vm.Subject}
+        Mesage: {vm.Message}
+        ", receivers: [userEmail!]);
+
+        TempData[TempDataKeys.ALERT_SUCCESS] = "Thank you for your interest. Your message has been sent!";
+
+        return RedirectToAction(nameof(Index));
     }
 }
+
--- Entities/ContactMeModel.cs
+++ Entities/ContactMeModel.cs
@@ -4,15 +4,15 @@ namespace Portfolio.Entities;
 
 public class ContactMeModel
 {
-    [Required(ErrorMessage = "Please enter your name: "), StringLength(32), Display(Name = "Name:")]
+    [Required(ErrorMessage = "Please enter your name"), StringLength(32), Display(Name = "Name")]
     public string? Name { get; set; }
 
-    [Required, StringLength(32), EmailAddress(ErrorMessage = "Please enter a valid email address:")]
+    [Required, StringLength(32), EmailAddress(ErrorMessage = "Please enter a valid email address")]
     public string? Email { get; set; }
 
-    [Required, StringLength(32)]
+    [Required(ErrorMessage = "Please enter a subject"), StringLength(32)]
     public string? Subject { get; set; }
 
-    [Required, StringLength(2048), MinLength(25, ErrorMessage = "Minimum length is 100 characters"), MaxLength(2048, ErrorMessage = "Maximum length is 2048 characters")]
+    [Required, StringLength(2048), MinLength(100, ErrorMessage = "Minimum length is 100 characters"), MaxLength(2048, ErrorMessage = "Maximum length is 2048 characters")]
     public string? Message { get; set; }
 }
